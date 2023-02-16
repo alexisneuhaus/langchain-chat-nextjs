3f1fe16141da3589c798802f06aedd63f4789226
alexisneuhaus <122240376+alexisneuhaus@users.noreply.github.com>
2023-02-16 10:24:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title></title><img src="https://langchain-chat-nextjs.vercel.app/manifestorlogo.png" />
+        <title>Manifestor</title>
         <meta name="description" content="LangChain documentation chatbot" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
